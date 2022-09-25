f0a29b0a85f4f35876fa5e9c420f203cfd574bef
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-25 16:34:12 +0330

    Update home layou

--- src/pages/layouts/home.layout.jsx
+++ src/pages/layouts/home.layout.jsx
@@ -1,9 +1,10 @@
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import { Box } from "@mui/material";
+import { Box, Drawer, Container, Toolbar } from "@mui/material";
 
 import Navbar from "../../components/navbar.component.";
+import DrawerComponent from "../../components/drawer.component";
 
 const HomeLayout = (props) => {
     const { children } = props;
@@ -13,9 +14,53 @@ const HomeLayout = (props) => {
     !session && history.push("/auth");
 
     return (
-        <Box>
+        <Box sx={{ display: 'flex' }}>
             <Navbar />
-            { children }
+            {
+                session
+                &&
+                <Drawer
+                    variant="permanent"
+                    anchor="right"
+                    sx={{
+                        '& .MuiDrawer-paper': {
+                            boxSizing: 'border-box',
+                            width: 300,
+                            flexShrink: 0,
+                            borderColor: "primary.main",
+                            bgcolor: "background.default",
+                            borderTopLeftRadius: 30,
+                            borderBottomLeftRadius: 30,
+                            direction: "rtl"
+                        },
+                    }}
+                >
+                    <DrawerComponent />
+                </Drawer>
+            }
+            {
+                session
+                ?
+                <Box
+                    component="main"
+                    sx={{
+                        flexGrow: 1,
+                        pr: 38,
+                    }}
+                >
+                    <Toolbar />
+                    <Container
+                        maxWidth="xl"
+                        sx={{
+                            my: 3,
+                        }}
+                    >
+                        { children }
+                    </Container>
+                </Box>
+                :
+                children
+            }
         </Box>
     );
 }
